d1afe031116536582387bafa43664398d4b12914
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-01-30 13:49:41 +0100

    reseting empty result handler

--- collector/collector.go
+++ collector/collector.go
@@ -67,15 +67,15 @@ func StartCollector(term string, location string, country string) string {
 
 	collector.Visit(baseURL + queryURL)
 
-	// if jobCount == "" {
-	// 	e := models.Err{
-	// 		Message: "There is no positions for this job 🙁",
-	// 	}
+	if jobCount == "" {
+		e := models.Err{
+			Message: "There is no positions for this job 🙁",
+		}
 
-	// 	err, _ := json.Marshal(e)
+		err, _ := json.Marshal(e)
 
-	// 	return string(err)
-	// }
+		return string(err)
+	}
 
 	j := models.Job{
 		Tech:     term,
